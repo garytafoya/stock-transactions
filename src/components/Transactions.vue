a517d8ce898845e4928133037b4872f23a6edd6d
Gary <gary_tafoya@yahoo.com>
2022-02-20 14:31:56 -0500

    test

---
+++ src/components/Transactions.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-row>
+     <b-table class="tableFont" striped hover :items="items" small table-variant="success" ></b-table>
+    </b-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          {buyDate:'03/28/2023', buyTime: '8:00am', symbol:'OCGN', shares:'100', buyPrice: '5.00', buyTotal:'1000', sellPrice:'6.00', sellDate: '03/28/2022', sellTime: '8:05am',sellTotal: '3000', percentGain: '5%', gain: '400', balance: '10000'},
+          {buyDate:'03/28/2022', buyTime: '8:00am', symbol:'OCGN', shares:'100', buyPrice: '5.00', buyTotal:'1000', sellPrice:'6.00', sellDate: '03/28/2022', sellTime: '8:05am', sellTotal: '3000', percentGain: '5%', gain: '400', balance: '10000'},
+          {buyDate:'03/28/2022', buyTime: '8:00am', symbol:'OCGN', shares:'100', buyPrice: '5.00', buyTotal:'1000', sellPrice:'6.00', sellDate: '03/28/2022', sellTime: '8:05am', sellTotal: '3000', percentGain: '5%', gain: '400', balance: '10000'}
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+.tableFont {
+	font-size: 0.75rem;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hi Gary1111</h1>
+    <transactions></transactions>
+  </div>
+</template>
+
+<script>
+import Transactions from "../components/Transactions.vue"
+  export default {
+    components: {
+      Transactions,
+    }
+  }
+</script>
